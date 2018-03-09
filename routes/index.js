b8f3183a7ebcb363e3fa6a04db07e48c605ec679
benjaminmballa <mballabenjamin@yahoo.fr>
2018-03-09 12:20:19 +0100

    second commit

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.json({ hi: 'Hello world'});
+  res.json({ hi: 'Hello world benjy'});
 });
 
 module.exports = router;
